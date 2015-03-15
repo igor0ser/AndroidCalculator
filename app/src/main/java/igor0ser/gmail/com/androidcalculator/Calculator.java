a780dfd99270d89ca3e8bf52a1a14ddbe9448c91
igor0ser <igor0ser@gmail.com>
2015-03-15 16:51:25 +0200

    second commit 15.03.2015

--- app/src/main/java/igor0ser/gmail/com/androidcalculator/Calculator.java
+++ app/src/main/java/igor0ser/gmail/com/androidcalculator/Calculator.java
@@ -1,38 +1,141 @@
 package igor0ser.gmail.com.androidcalculator;
 
-/**
- * Created by Admin on 13.03.2015.
- */
+import java.text.NumberFormat;
+import java.text.ParseException;
+
 public class Calculator {
-    double res;
-    double res1;
-    char operator = '+';
-    boolean isBeforeDot = true;
-    String result;
-    StringBuilder sb;
-    int dec = 1;
+    private StringBuilder sb;
+    private NumberFormat nf;
+    public String display;
+    private double result;
+    private char operator = '+';
+    private boolean haveItDot = false;
+    private static final double MAX = 999999999;
+    private double d;
+    private boolean flag = false;
 
-    void setResult(int x){
-        if (isBeforeDot)
-        res1 = res1*10 + x;
-        else {
-        res1 = res1 + x/Math.pow(10,dec);
-            dec++;}
-        result = Double.toString(res1);
-    }
-    void AC(){
-        res1=0;
-        result = Double.toString(res1);
+    public Calculator() {
+        sb = new StringBuilder(12);
+        sb.append('0');
+
+        nf = NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(9);
+        nf.setGroupingUsed(false);
     }
 
-    void dot(){
-        if (isBeforeDot){
-        isBeforeDot=false;
-        result = result + ".";}
+    private boolean isLengthOk() {
+        if (haveItDot)
+            return (sb.length() <= 9);
+        else
+            return (sb.length() <= 8);
     }
 
-    String result(){
-        return Double.toString(res);
+    public void addDigit(char c) {
+        if (flag)
+            result = 0;
+        if (isLengthOk()) {
+            if (sb.toString().equals("0"))
+                sb.deleteCharAt(0);
+            sb.append(c);
+            display = sb.toString();
+        }
+    }
+
+    public void addDot() {
+        if (flag)
+            result = 0;
+        if ((!haveItDot) && (isLengthOk())) {
+            sb.append(',');
+            haveItDot = true;
+            display = sb.toString();
+        }
+    }
+
+    public void AC() {
+        sb.delete(0, sb.length());
+        sb.append('0');
+        display = sb.toString();
+        result = 0;
+        haveItDot = false;
+        flag = false;
+        operator = '+';
+
+    }
+
+    private void action() {
+        if (!flag) {
+            d = parseToDouble(sb);
+            // operator = '+';
+        }
+
+        switch (operator) {
+            case '+':
+                result = result + d;
+                break;
+            case '-':
+                result = result - d;
+                break;
+            case '/':
+                result = result / d;
+                break;
+            case '*':
+                result = result * d;
+                break;
+        }
+
+        display = toDisplay(result);
+        sb.delete(0, sb.length());
+        sb.append('0');
+        haveItDot = false;
+    }
+
+    public void execute(char c) {
+        if (!flag) {
+            action();
+        }
+        flag = false;
+        operator = c;
+    }
+
+    public void buttonEquals() {
+        action();
+        // operator = '+';
+        flag = true;
+    }
+
+    private String toDisplay(double d) {
+
+        if ((d > MAX) || (d < -MAX)) {
+            result = 0;
+            return "error";
+        } else {
+            String s = nf.format(d);
+            if (s.length() > 9) {
+                s = s.substring(0, 9);
+                if (s.endsWith(","))
+                    s = s.substring(0, 8);
+            }
+            return s;
+        }
+    }
+
+    private double parseToDouble(StringBuilder s) {
+
+        String s1 = s.toString();
+        double d = 0;
+        if (haveItDot == true) {
+            if (s1.endsWith(","))
+                s1 = s1.substring(0, s.length() - 1);
+
+            try {
+                d = (double) nf.parseObject(sb.toString());
+            } catch (ParseException e) {
+                System.err.println("something goes wrong with parsing.");
+            }
+
+        } else if (haveItDot == false)
+            d = Double.parseDouble(s1);
+        return d;
     }
 
 }
--- app/src/main/java/igor0ser/gmail/com/androidcalculator/MainActivity.java
+++ app/src/main/java/igor0ser/gmail/com/androidcalculator/MainActivity.java
@@ -22,14 +22,12 @@ public class MainActivity extends ActionBarActivity {
     Button button7;
     Button button8;
     Button button9;
-    Button buttonPlus;
-    Button buttonMinus;
+    Button buttonAdd;
+    Button buttonSubtract;
     Button buttonMultiply;
     Button buttonDivide;
     Button buttonEquals;
     Button buttonAC;
-    Button buttonSign;
-    Button buttonPercent;
     Button buttonDot;
     Calculator calc;
 
@@ -47,125 +45,127 @@ public class MainActivity extends ActionBarActivity {
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button7);
-        button5 = (Button) findViewById(R.id.buttonAC);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
         button6 = (Button) findViewById(R.id.button6);
         button7 = (Button) findViewById(R.id.button7);
-        button8 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
         button9 = (Button) findViewById(R.id.button9);
-        buttonPlus = (Button) findViewById(R.id.buttonPlus);
-        buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonSubtract = (Button) findViewById(R.id.buttonSubtract);
         buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
         buttonDivide = (Button) findViewById(R.id.buttonDivide);
         buttonEquals = (Button) findViewById(R.id.buttonEquals);
         buttonAC = (Button) findViewById(R.id.buttonAC);
-        buttonSign = (Button) findViewById(R.id.buttonSign);
-        buttonPercent = (Button) findViewById(R.id.buttonPercent);
         buttonDot = (Button) findViewById(R.id.buttonDot);
         result = (TextView) findViewById(R.id.result);
         result.setText("0");
 
         OnClickListener listener0 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(0);
-                result.setText(calc.result);
+                calc.addDigit('0');
+                result.setText(calc.display);
             }
            };
         OnClickListener listener1 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(1);
-                result.setText(calc.result);
+                calc.addDigit('1');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener2 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(2);
-                result.setText(calc.result);
+                calc.addDigit('2');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener3 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(3);
-                result.setText(calc.result);
+                calc.addDigit('3');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener4 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(4);
-                result.setText(calc.result);
+                calc.addDigit('4');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener5 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(5);
-                result.setText(calc.result);
+                calc.addDigit('5');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener6 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(6);
-                result.setText(calc.result);
+                calc.addDigit('6');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener7 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(7);
-                result.setText(calc.result);
+                calc.addDigit('7');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener8 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(8);
-                result.setText(calc.result);
+                calc.addDigit('8');
+                result.setText(calc.display);
             }
         };
         OnClickListener listener9 = new OnClickListener(){
             public void onClick(View w){
-                calc.setResult(9);
-                result.setText(calc.result);
+                calc.addDigit('9');
+                result.setText(calc.display);
             }
         };
-        OnClickListener listenerPlus = new OnClickListener(){
+        OnClickListener listenerAdd = new OnClickListener(){
             public void onClick(View w){
+                calc.execute('+');
+                result.setText(calc.display);
             }
         };
-        OnClickListener listenerMinus = new OnClickListener(){
+        OnClickListener listenerSubtract = new OnClickListener(){
             public void onClick(View w){
+                calc.execute('-');
+               result.setText(calc.display);
             }
         };
         OnClickListener listenerMultiply = new OnClickListener(){
             public void onClick(View w){
+                calc.execute('*');
+                result.setText(calc.display);
             }
         };
         OnClickListener listenerDivide = new OnClickListener(){
             public void onClick(View w){
+                calc.execute('/');
+                result.setText(calc.display);
             }
         };
         OnClickListener listenerEquals = new OnClickListener(){
             public void onClick(View w){
+                calc.buttonEquals();
+                result.setText(calc.display);
             }
         };
         OnClickListener listenerAC = new OnClickListener(){
             public void onClick(View w){
                 calc.AC();
-                result.setText(calc.result);
+                result.setText(calc.display);
             }
         };
-        OnClickListener listenerSign= new OnClickListener(){
-            public void onClick(View w){
-            }
-        };
-        OnClickListener listenerPercent= new OnClickListener(){
-            public void onClick(View w){
-            }
-        };
+
         OnClickListener listenerDot= new OnClickListener(){
 
             public void onClick(View w){
-                calc.dot();
-                result.setText(calc.result);
+                calc.addDot();
+                result.setText(calc.display);
             }
         };
+
 
         button0.setOnClickListener(listener0);
         button1.setOnClickListener(listener1);
@@ -177,14 +177,12 @@ public class MainActivity extends ActionBarActivity {
         button7.setOnClickListener(listener7);
         button8.setOnClickListener(listener8);
         button9.setOnClickListener(listener9);
-        buttonPlus.setOnClickListener(listenerPlus);
-        buttonMinus.setOnClickListener(listenerMinus);
+        buttonAdd.setOnClickListener(listenerAdd);
+        buttonSubtract.setOnClickListener(listenerSubtract);
         buttonDivide.setOnClickListener(listenerDivide);
         buttonMultiply.setOnClickListener(listenerMultiply);
         buttonEquals.setOnClickListener(listenerEquals);
         buttonAC.setOnClickListener(listenerAC);
-        buttonSign.setOnClickListener(listenerSign);
-        buttonPercent.setOnClickListener(listenerPercent);
         buttonDot.setOnClickListener(listenerDot);
 
 
